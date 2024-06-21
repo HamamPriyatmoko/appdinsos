e4d9e94e7e10af0e37e130e53343c44ca0185443
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2024-06-21 17:46:30 +0700

    first commit

---
+++ src/main/java/appdinsos/appdinsos/AppdinsosApplication.java
@@ -0,0 +1,13 @@
+package appdinsos.appdinsos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppdinsosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppdinsosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/appdinsos/appdinsos/AppdinsosApplicationTests.java
@@ -0,0 +1,13 @@
+package appdinsos.appdinsos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppdinsosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
